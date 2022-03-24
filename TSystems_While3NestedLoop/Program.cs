f2bde42e6b33cd4433fd80e4b58f7873c288fa79
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:44:54 +0530

    Add project files.

---
+++ TSystems_While3NestedLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_While3NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j, k;
+            while (i<=3)
+            {
+                j = 1;
+                while (j<=3)
+                {
+                    k = 1;
+                    while (k<=3)
+                    {
+                        Console.WriteLine(i+"  "+j+" "+k);
+                        k++;
+                    }
+                    j++;
+                }
+                i++;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
